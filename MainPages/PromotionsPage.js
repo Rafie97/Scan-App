9e2097e4675966b65ec4f1ac15abae28917e3a05
Rafael Josh <rajosh80@gmail.com>
2020-11-04 13:52:22 -0600

    Promotions page update

--- MainPages/PromotionsPage.js
+++ MainPages/PromotionsPage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Image, ImageBackground, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, View, Text, Image, ImageBackground, TouchableOpacity, FlatList } from 'react-native';
 import Grid from 'react-native-grid-component';
 import firestore from '@react-native-firebase/firestore';
 import Item from '../Models/Item';
+import FamilyTile from '../Models/Components/FamilyTile';
 
 
 class PromotionsPage extends Component {
@@ -24,31 +25,42 @@ class PromotionsPage extends Component {
 
   async getItems() {
     const hebRef = firestore().collection("stores").doc("HEB").collection('items')
-    
 
-    const sub = hebRef.onSnapshot((snap) => {
+    hebRef.onSnapshot((snap) => {
       this.setState({ promoItems: [] })
       snap.forEach(async (doc) => {
         const item = new Item(doc);
         await this.setState({ promoItems: [...this.state.promoItems, item] });
       })
     })
-
-    return () => sub();
-
   }
 
 
   render() {
+    
+    let arr = [];
+    this.state.promoItems.forEach((i)=>{
+      arr = [...arr, i.name];
+    })
+
+    console.log('Arrrrr', arr);
+
     return (
       <ImageBackground source={require('../res/android-promotions.png')} style={styles.fullBackground} >
         <View style={styles.promoPageContainer} >
 
           <Text style={styles.promoTitle}>Today's Best Deals</Text>
 
-          
-          <Grid style={styles.gridContainer} renderItem={this.renderItem} data={this.state.promoItems} numColumns={2} renderPlaceholder={(i)=>(<View style={{flex:1, height:60, width:400, backgroundColor:'yellow'}} />)} />
-            
+          <Text>Explore our best picks</Text>
+          <View style={{height:200, marginTop:20}}>
+            <FlatList  data={['Weekly deals', 'Fall flavors', 'Halloween Specials', 'Savings on appliances']} horizontal={true} renderItem={({ item }) => (<FamilyTile name={item} />)} />
+          </View>
+          <Text>Explore our coupons</Text>
+          <FlatList data={this.state.promoItems} horizontal={true} renderItem={this.renderItem}  />
+
+
+          {//<Grid style={styles.gridContainer} renderItem={this.renderItem} data={this.state.promoItems} numColumns={2} renderPlaceholder={(i)=>(<View style={{flex:1, height:60, width:400, backgroundColor:'yellow'}} />)} />
+  }
         </View>
       </ImageBackground>
 
@@ -56,13 +68,14 @@ class PromotionsPage extends Component {
   }
 
 
-  renderItem(data, i) {
+  renderItem = ({ item }) => {
     const { navigate } = this.props.navigation;
     return (
       
-        <TouchableOpacity style={styles.itemBox} activeOpacity={0.5} onPress={() => navigate('Promo', { screen: 'PromoItemPage', params: { itemIDCallback: this.state.promoItems[i] } })} >
-          <Image style={styles.itemImage} source={{ uri: this.state.promoItems[i].imageLink }} />
-          <Text style={styles.itemTitleText}>{this.state.promoItems[i].name}</Text>
+        <TouchableOpacity style={styles.itemBox} activeOpacity={0.5} onPress={() => navigate('Promo', { screen: 'PromoItemPage', params: { itemIDCallback: item } })} >
+          <Image style={styles.itemImage} source={{ uri: item.imageLink }} />
+        
+          <Text style={styles.itemTitleText}>{item.name}</Text>
         </TouchableOpacity>
   
     )
@@ -117,11 +130,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     borderColor: 'grey',
-    marginRight: 20,
-    marginLeft: 20,
-    marginBottom: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    marginTop: 20,
     flex: 1,
-    maxWidth: 160
+    maxWidth: 180,
+    maxHeight:160,
   },
 
   itemTitleText: {
