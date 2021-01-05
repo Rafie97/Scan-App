a9d040e375f48a8e25eaea9e56bc1485f8966983
Rafa Josh <rajosh80@gmail.com>
2021-01-04 23:53:36 -0600

    reboot

--- Models/Item.js
+++ Models/Item.js
@@ -1,34 +1,47 @@
 class Item {
-    constructor(doc) {
-        if(typeof doc.data =='function'){
-            this.docID = doc.id;
-            this.name = doc.data().name;
-            this.price = doc.data().price;
-            this.imageLink = doc.data().imageLink;
-            this.barcode = doc.data().barcode;
-            this.promo = doc.data().promo;
-            this.reviews = doc.data().reviews;
-            this.priceHistory = doc.data().priceHistory;
-            this.location = doc.data().location;
-        }
-        else{
-            this.docID = doc.docID;
-            this.name = doc.name;
-            this.price = doc.price;
-            this.imageLink = doc.imageLink;
-            this.barcode = doc.barcode;
-            this.promo = doc.promo;
-            this.reviews = doc.reviews;
-            this.priceHistory = doc.priceHistory;
-            this.location = doc.location;
-        }
+  constructor(doc) {
+    if (typeof doc.data == 'function') {
+      this.docID = doc.id;
+      this.name = doc.data().name;
+      this.price = doc.data().price;
+      this.imageLink = doc.data().imageLink;
+      this.barcode = doc.data().barcode;
+      this.promo = doc.data().promo;
+      this.reviews = doc.data().reviews;
+      this.priceHistory = doc.data().priceHistory;
+      this.location = doc.data().location;
+    } else {
+      this.docID = doc.docID;
+      this.name = doc.name;
+      this.price = doc.price;
+      this.imageLink = doc.imageLink;
+      this.barcode = doc.barcode;
+      this.promo = doc.promo;
+      this.reviews = doc.reviews;
+      this.priceHistory = doc.priceHistory;
+      this.location = doc.location;
     }
+  }
 
-    toString() {
-        return this.docID + ',' + this.name + ',' + this.price + ',' + this.imageLink + ',' + this.barcode + ',' + this.promo + ',' + this.reviews + ',' + this.priceHistory;
-    }
-
+  toString() {
+    return (
+      this.docID +
+      ',' +
+      this.name +
+      ',' +
+      this.price +
+      ',' +
+      this.imageLink +
+      ',' +
+      this.barcode +
+      ',' +
+      this.promo +
+      ',' +
+      this.reviews +
+      ',' +
+      this.priceHistory
+    );
+  }
 }
-
 
 export default Item;
