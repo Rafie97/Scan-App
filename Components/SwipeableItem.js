f7566e0d284ad2e2defc7042bde1fb15f436991d
rafa <rajosh80@gmail.com>
2021-08-31 22:31:36 -0500

    Make swipeableitem functional

--- Components/SwipeableItem.js
+++ Components/SwipeableItem.js
@@ -14,142 +14,131 @@ import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 const {width} = Dimensions.get('window');
 
-export default class SwipeableItem extends React.PureComponent {
-  constructor(props) {
-    super(props);
+function SwipeableItem(props) {
+  const [scrollViewEnabled, setScrollEnabled] = React.useState(true);
+  const position = React.useRef(new Animated.ValueXY()).current;
 
-    this.gestureDelay = 10;
-    this.scrollViewEnabled = true;
-
-    const position = new Animated.ValueXY();
-    this.state = {position};
-
-    const panResponder = PanResponder.create({
+  const gestureDelay = 10;
+  const panResponder = React.useRef(
+    PanResponder.create({
       onStartShouldSetPanResponder: (evt, gestureState) => false,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onPanResponderTerminationRequest: (evt, gestureState) => false,
       onPanResponderMove: (evt, gestureState) => {
         if (gestureState.dx < -15) {
-          this.setScrollViewEnabled(false);
-          let newX = gestureState.dx + this.gestureDelay;
+          setScrollViewEnabled(false);
+          let newX = gestureState.dx + gestureDelay;
           position.setValue({x: newX, y: 0});
         }
       },
       onPanResponderRelease: (evt, gestureState) => {
         if (gestureState.dx < -50) {
-          Animated.timing(this.state.position, {
+          Animated.timing(position, {
             toValue: {x: -50, y: 0},
             duration: 150,
           }).start(() => {
-            this.setScrollViewEnabled(true);
+            setScrollViewEnabled(true);
           });
         }
         if (gestureState.dx > -50) {
-          Animated.timing(this.state.position, {
+          Animated.timing(position, {
             toValue: {x: 0, y: 0},
             duration: 150,
           }).start(() => {
-            this.setScrollViewEnabled(true);
+            setScrollViewEnabled(true);
           });
         }
       },
-    });
+    }),
+  ).current;
 
-    this.panResponder = panResponder;
-    this.navToItem = this.navToItem.bind(this);
-  }
-
-  setScrollViewEnabled(enabled) {
-    if (this.scrollViewEnabled !== enabled) {
-      this.props.setScrollEnabled(enabled);
-      this.scrollViewEnabled = enabled;
+  function setScrollViewEnabled(enabled) {
+    if (scrollViewEnabled !== enabled) {
+      props.setScrollEnabled(enabled);
+      setScrollEnabled(enabled);
     }
   }
 
-  navToItem() {
-    this.props.navigation.navigate(this.props.sourcePage, {
-      screen: this.props.sourcePage + 'ItemPage',
-      params: {itemIDCallback: this.props.item},
+  function navToItem() {
+    props.navigation.navigate(props.sourcePage, {
+      screen: props.sourcePage + 'ItemPage',
+      params: {itemIDCallback: props.item},
     });
   }
 
-  render() {
-    console.log(this.state.position);
-    return (
-      <View>
+  return (
+    <View>
+      <Animated.View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+        }}>
+        <View
+          style={{
+            alignSelf: 'center',
+            paddingBottom: 20,
+            position: 'absolute',
+            right: -5,
+          }}>
+          <TouchableOpacity onPress={() => props.deleteItem(props.item.docID)}>
+            <Animated.View
+              style={{
+                backgroundColor:
+                  position.x < -15 ? 'rgba(217, 63, 18, 0.5)' : '#D93F12', //'#D93F12'
+                width: 40,
+                height: 40,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 10,
+                borderWidth: 2,
+              }}>
+              <EvilIcons name="trash" size={30} color="black" />
+            </Animated.View>
+          </TouchableOpacity>
+        </View>
+
         <Animated.View
           style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-          }}>
-          <View
-            style={{
-              alignSelf: 'center',
-              paddingBottom: 20,
-              position: 'absolute',
-              right: -5,
-            }}>
+            left: position.x,
+          }}
+          {...panResponder.panHandlers}>
+          <View style={styles.itemBubble}>
+            <Image
+              source={{uri: props.item.imageLink}}
+              style={styles.itemImage}
+            />
             <TouchableOpacity
-              onPress={() => this.props.deleteItem(this.props.item.docID)}>
-              <Animated.View
-                style={{
-                  backgroundColor:
-                    this.state.position.x < -15
-                      ? 'rgba(217, 63, 18, 0.5)'
-                      : '#D93F12', //'#D93F12'
-                  width: 40,
-                  height: 40,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  borderRadius: 10,
-                  borderWidth: 2,
-                }}>
-                <EvilIcons name="trash" size={30} color="black" />
-              </Animated.View>
+              onPress={navToItem}
+              activeOpacity={0.5}
+              style={{
+                marginVertical: 10,
+                width: '40%',
+                height: '75%',
+              }}>
+              <Text style={styles.itemLabel}>{props.item.name}</Text>
             </TouchableOpacity>
-          </View>
-
-          <Animated.View
-            style={{
-              left: this.state.position.x,
-            }}
-            {...this.panResponder.panHandlers}>
-            <View style={styles.itemBubble}>
-              <Image
-                source={{uri: this.props.item.imageLink}}
-                style={styles.itemImage}
-              />
-              <TouchableOpacity
-                onPress={this.navToItem}
-                activeOpacity={0.5}
-                style={{
-                  marginVertical: 10,
-                  width: '40%',
-                  height: '75%',
-                }}>
-                <Text style={styles.itemLabel}>{this.props.item.name}</Text>
-              </TouchableOpacity>
-              <View
-                style={{
-                  flexDirection: 'column',
-                  alignSelf: 'center',
-                }}>
-                <View style={{flexDirection: 'row', marginBottom: 5}}>
-                  <EvilIcons name="minus" size={30} />
-                  <Text style={{alignSelf: 'center', marginHorizontal: 2}}>
-                    x2
-                  </Text>
-                  <EvilIcons name="plus" size={30} />
-                </View>
-                <Text style={styles.itemPrice}>${this.props.item.price}</Text>
+            <View
+              style={{
+                flexDirection: 'column',
+                alignSelf: 'center',
+              }}>
+              <View style={{flexDirection: 'row', marginBottom: 5}}>
+                <EvilIcons name="minus" size={30} />
+                <Text style={{alignSelf: 'center', marginHorizontal: 2}}>
+                  x2
+                </Text>
+                <EvilIcons name="plus" size={30} />
               </View>
+              <Text style={styles.itemPrice}>${props.item.price}</Text>
             </View>
-          </Animated.View>
+          </View>
         </Animated.View>
-      </View>
-    );
-  }
+      </Animated.View>
+    </View>
+  );
 }
+
+export default SwipeableItem;
 
 const styles = StyleSheet.create({
   itemBubble: {
